ba2d43698f5108d8b0f2e75c85b555f2da365f8b
satrioalifw <satrioalifw@gmail.com>
2023-10-23 16:31:44 +0700

    commit success login

--- cypress/e2e/3-web-testing/Login.cy.js
+++ cypress/e2e/3-web-testing/Login.cy.js
@@ -1,5 +1,10 @@
 describe('Web UI Magento', () => {
-    it('Login Magento', () => {
+    beforeEach(() => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
-        cy.get('#email')').type('email')
-        cy.get('#pass')').type('password')
+    })
+    it('Login Magento', () => {
+        cy.get('#email').type('email')
+        cy.get('#pass').type('password')
+        cy.get('#send2').click()
+    })
+})
