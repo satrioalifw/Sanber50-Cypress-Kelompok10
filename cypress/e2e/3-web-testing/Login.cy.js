f45b5795129eb7a591d7760afd95a1ee33c17a71
melanicananda <melanicanandatr@gmail.com>
2023-10-28 23:16:56 +0700

    Update Success and Failed Login

--- cypress/e2e/3-web-testing/Login.cy.js
+++ cypress/e2e/3-web-testing/Login.cy.js
@@ -2,9 +2,16 @@ describe('Web UI Magento', () => {
     beforeEach(() => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
     })
-    it('Login Magento', () => {
-        cy.get('#email').type('email')
-        cy.get('#pass').type('password')
+    it('Success Login Magento', () => {
+        cy.get('#email').type('melanicanandatr@gmail.com')
+        cy.get('#pass').type('Melan123#')
         cy.get('#send2').click()
+        cy.get('.logo').should('be.visible', 'LUMA')
+    })
+    it('Failed_Login', () => {
+        cy.get('#email').type('melanicanandatr@gmail.com')
+        cy.get('#pass').type('melan123')
+        cy.get('#send2').click()
+        cy.get('[data-bind="html: $parent.prepareMessageForHtml(message.text)"]').should('be.visible')
     })
 })
